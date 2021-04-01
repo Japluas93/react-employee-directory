ebd70420c3c9ff00c39a31351fefbd07e019fce2
Julian Pluas <julianpluas@gmail>
2021-03-31 20:51:24 -0700

    Created the employee component

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,1 +1,25 @@
 import React from "react";
+
+const Employee = ({
+  text,
+  employee,
+  employees,
+  setEmployees,
+  data,
+  setData,
+}) => {
+  const deleteHandler = () => {
+    setEmployees(employees.filter((el) => el.id !== employee.id));
+  };
+
+  return (
+    <div>
+      <li>{employee.name.first}</li>
+      <li>{employee.name.last}</li>
+      <li>{employee.cell}</li>
+      <button onClick={deleteHandler}></button>
+    </div>
+  );
+};
+
+export default Employee;
