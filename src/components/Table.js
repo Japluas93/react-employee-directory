cb3a9c571adba7aee52c53c45f4e63d6798c4baf
Julian Pluas <julianpluas@gmail>
2021-03-31 20:57:09 -0700

    Created employee component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,2 +1,27 @@
 import React from "react";
 import Employee from "./Employee";
+const Table = ({
+  employees,
+  setEmployees,
+  filteredEmployees,
+  data,
+  setData,
+}) => {
+  return (
+    <div className="employee-container">
+      <ul className="employee-list">
+        {data.map((employee) => (
+          <Employee
+            setEmployees={setEmployees}
+            setData={setData}
+            data={data}
+            employees={employees}
+            employee={employee}
+            text={employee.text}
+            key={employee.cell}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
