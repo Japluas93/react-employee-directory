65865dfe6bb3e541e5b82a60fe17ef885ebce734
Julian Pluas <julianpluas@gmail>
2021-03-31 21:06:21 -0700

    Created the Form component

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+const Form = ({
+  setInputText,
+  employees,
+  setEmployees,
+  inputText,
+  setStatus,
+}) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitEmployeeHandler = (e) => {
+    e.preventDefault();
+    setEmployees([
+      ...employees,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <form>
+      <input value={inputText} onChange={inputTextHandler} type="text" />
+      <button onClick={submitEmployeeHandler} type="submit"></button>
+      <div>
+        <select onChange={statusHandler} name="todos">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
